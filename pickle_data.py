05fe306b80d036fee3de4a94d4cbf89b1517a928
Johnson He <hejohns@umich.edu>
2024-04-24 06:54:06 +0000

    okay maybe this is finally not stupid

--- pickle_data.py
+++ pickle_data.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# pickle entire dataset as grandUnifiedData
+# and store indices for windows in windows
+
 import pandas
 import sys
 import os
@@ -7,14 +10,18 @@ import pickle
 import re
 import numpy as np
 
+from config import *
+
 assert len(sys.argv) == 3
 subjects = [f for f in os.listdir(os.getcwd()) if re.search("^AB\d+$", f)]
 activities_re = re.compile(".")
 grandUnifiedData = {}
+windows = []
 for s in subjects:
     grandUnifiedData[s] = {}
     activities = [f for f in os.listdir(os.path.join(os.getcwd(), s)) if activities_re.search(f)]
     for a in activities:
+        # get dataframe for this [subject][activity]
         leaf = os.path.join(os.getcwd(), s, a, sys.argv[1])
         assert os.path.isfile(leaf)
         df = pandas.read_csv(leaf, index_col="time")
@@ -23,5 +30,10 @@ for s in subjects:
         # https://stackoverflow.com/a/69188251
         df[df.select_dtypes(np.float64).columns] = df.select_dtypes(np.float64).astype(np.float32)
         grandUnifiedData[s][a] = df
+        # calculate window indices
+        for i in range(len(df.index) - window_size):
+            dg = df.iloc[i : i + window_size]
+            if not dg.isnull().values.any():
+                windows.append((s, a, i))
 with open(sys.argv[2], 'wb') as f:
-    pickle.dump(grandUnifiedData, f)
+    pickle.dump((grandUnifiedData, windows), f)
