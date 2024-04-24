8c58f0095b4260c1521cb3807f875c0d02f021db
Johnson He <hejohns@umich.edu>
2024-04-24 23:37:35 +0000

    try greedy this way

--- dataloader.py
+++ dataloader.py
@@ -40,3 +40,32 @@ class GrandLSTMDataset(Dataset):
         assert not sample_t.isnan().any()
         assert not label_t.isnan().any()
         return sample_t, label_t
+
+# precompute all windows and try to fit them all in memory
+class GreedyGrandLSTMDataset(Dataset):
+    def __init__(self, subjects, activities, pickled_data="GrandUnifiedData.pickle"):
+        with open(pickled_data, 'rb') as f:
+            grandUnifiedData, windows = pickle.load(f)
+        # save some memory if we have lots of concurrent datasets
+        # NOTE: yes this is repeated for every GrandLSTMDataset, but hopefully
+        # it's not too slow
+        for s in list(grandUnifiedData.keys()):
+            if s not in subjects:
+                del grandUnifiedData[s]
+        # if subjects or activities, windows needs to be corrected, by dropping
+        # any windows that are for other subjects or activities
+        windows = [(s, a, i) for s, a, i in windows if s in subjects and activities.search(a)]
+        self.windows = []
+        for s, a, i in window_indices:
+            window = grandUnifiedData[s][a].iloc[i : i + window_size]
+            sample_df, label_df = window[sensor_list], window[output_list]
+            sample_t, label_t = torch.tensor(sample_df.to_numpy(dtype=np.float32)), torch.tensor(label_df.to_numpy(dtype=np.float32))
+            # these asserts should be redudant, since we should've filtered out any
+            # windows with NaNs while generating grandUnifiedData
+            assert not sample_t.isnan().any()
+            assert not label_t.isnan().any()
+            self.windows.append((sample_t, label_t))
+    def __len__(self):
+        return len(self.windows)
+    def __getitem__(self, idx):
+        return self.windows[idx]
