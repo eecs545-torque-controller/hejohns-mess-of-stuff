919a29858fdd3447a0b4612a4cc7ec0dbeb1a3ad
Johnson He <hejohns@umich.edu>
2024-04-26 07:04:07 +0000

    bump

--- dataloader.py
+++ dataloader.py
@@ -91,7 +91,7 @@ class GreedyGrandLSTMDataset(Dataset):
         grandUnifiedData, self.columnwise_sum, self.columnwise_count, self.columnwise_std = normalize_data(grandUnifiedData)
         print(f"normalize_data finished at {curtime()}")
         print(f"starting to get_window ... {curtime()}")
-        with Pool(processes=min(len(os.sched_getaffinity(0), 8))) as pool:
+        with Pool(processes=min(len(os.sched_getaffinity(0)), 8)) as pool:
             #self.windows = [get_window(grandUnifiedData, s, a, i) for s, a, i in windows]
             self.windows = pool.map((lambda t: get_window(grandUnifiedData, t[0], t[1], t[2])), windows)
         print(f"get_window finished at {curtime()}")
