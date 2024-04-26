bc0a2ac2bc11d7bffcf4890b7297ae5ecc0e0a42
Johnson He <hejohns@umich.edu>
2024-04-26 21:52:57 +0000

    check

--- check_pickle.py
+++ check_pickle.py
@@ -8,15 +8,22 @@ import re
 from config import *
 from dataloader import *
 
-pickled_data = read_entire_pickle()
-subjects = pickled_data[0].keys()
-activities = re.compile(".");
-dataset = GrandLSTMDataset(pickled_data, subjects, activities)
-for s in dataset.grandUnifiedData.keys():
-    for a in dataset.grandUnifiedData[s].keys():
-        print(f"{s}/{a}")
-        print(dataset.grandUnifiedData[s][a])
-print("--------------------")
-for w in dataset.windows:
-    print(w)
-
+def check(filepath):
+    grandUnifiedData, windows, *normalization_params = read_entire_pickle(filepath=filepath)
+    print(f"dataset has {len(dataset.windows)} windows")
+    print("normalization_params: ", normalization_params)
+    subjects = grandUnifiedData.keys()
+    activities = re.compile(".");
+    dataset = GrandLSTMDataset(pickled_data, subjects, activities)
+    print("--------------------")
+    for s in dataset.grandUnifiedData.keys():
+        for a in dataset.grandUnifiedData[s].keys():
+            print(f"{s}/{a}")
+            print(dataset.grandUnifiedData[s][a])
+    print("--------------------")
+    for w in dataset.windows:
+        print(w)
+    print("--------------------")
+    
+check("GrandUnifiedData.pickle")
+check("GrandUnifiedData_normalized.pickle")
